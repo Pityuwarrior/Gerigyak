b52971c17453643a87f187b8a838d7b0a93e694b
Norbi <Norbi@DESKTOP-ATL7KS8>
2022-03-10 16:55:27 +0100

    Add project files.

---
+++ Gerigyak/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerigyak
+{
+    public class Alkalmazott
+    {
+       private string nev;
+       private int kor;
+       private int cicidb;
+
+        public Alkalmazott(string nev, int cicidb) 
+        {
+            this.nev = nev;
+            this.cicidb = cicidb;
+        }
+
+        public Alkalmazott(int kor) 
+        {
+            this.kor = kor;
+        }
+        
+        static void Main(string[] args)
+        {
+            Alkalmazott a = new Alkalmazott("Józsi", 7);
+            Alkalmazott b = new Alkalmazott(19);
+            Console.WriteLine(a.nev + " " + a.cicidb);;
+            Console.ReadKey();
+        }
+
+
+    }
+}
